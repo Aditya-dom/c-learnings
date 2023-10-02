89f08d79bc6b00f6d9b07d5a2ff1eb9c52f5a62b
Aditya <70410645+Aditya-dom@users.noreply.github.com>
2023-10-02 20:33:45 +0530

    preprocessor egs

---
+++ preprocessor in c.c
@@ -0,0 +1,19 @@
+// some preprocessor commands egs
+/*      
+#define
+#include
+#undef
+#ifndef
+#if -- if any comile time conditn is ture
+#else
+#elif
+*/
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+#include <stdlib.h>
+int main()
+{
+    
+    return 0;
+}
